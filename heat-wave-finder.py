945adab6e7487967bb9f6115c580904195f40fb5
Guilherme Abreu <guiii88@outlook.com>
2019-09-22 19:13:51 -0300

    add library that actualy finds heatwaves

--- heat-wave-finder.py
+++ heat-wave-finder.py
@@ -6,21 +6,16 @@ Created on Wed Sep 18 14:57:37 2019
 @author: guilherme
 """
 
-
 #importing libraries
 import pandas as pd
-import sys, os
-chpath = os.path.abspath('../climate_health')
-if chpath not in sys.path:
-    sys.path.append(chpath)
-import climate as ch
+import cm_library as cm
 
 dataset = pd.read_excel("../data_set/Dados_CEPAGRI/1997.xlsx")
 dataframe = dataset.loc[:, ~dataset.columns.str.contains('^Unnamed')]
-flag = '0/1 - not HW/ HW'
+flag = '0 (not HW)/ 1 (HW)'
 hw_name='HeatWaves'
 
-fv = ch.get_heatwave(data=dataframe, flag=flag, hw_name=hw_name, day_name = dataset.columns[2], year_name = dataset.columns[1],
-                     mean_tem_name = dataset.columns[12])
-main_col = [dataframe.columns[1],dataframe.columns[2],dataframe.columns[12],flag,hw_name, 'Percentil 90']
+fv = cm.get_heatwave(data=dataframe, flag=flag, hw_name=hw_name, day_name = dataset.columns[2], year_name = dataset.columns[1],
+                     mean_tmp_name = dataset.columns[12])
+main_col = [dataframe.columns[1],dataframe.columns[2],dataframe.columns[12],flag,hw_name, 'Percentile 90']
 res_df = fv[main_col]
