37bb0d0eeb070e0a5731bc3db12cc3895f37ceaf
Guilherme Abreu <guiii88@outlook.com>
2019-09-27 19:19:21 -0300

    It will be removed

--- ecb-pre-processing.py
+++ ecb-pre-processing.py
@@ -10,35 +10,39 @@ Created on Thu Sep 26 18:45:31 2019
 import pandas as pd
 import numpy as np
 
-file = open("../data_set/ecobee/Plinio's Dataset - December 2016 to August 10 2018/"+
-                        "House 1 - Iron Gate/Temperature/report-311001483636-2016-12-01-to-2016-12-31.csv", 'r')
-data = file.readlines()
-file.close()
-
-i = 0;
-while i < len(data):
-    if '#' in data[i] or '\n' == data[i] or '' == data[i]:
-        data.pop(i)
-    else:
-        i+=1
-        
-file = open("../data_set/ecobee/Plinio's Dataset - December 2016 to August 10 2018/"+
-                        "House 1 - Iron Gate/Temperature/report-2016-12-01-to-2016-12-31.csv", 'w')
-
-columns = data[0].split(',')
-
-for line in data:
-    file.write(line)
-file.close()
-
-data = [line.split(',')[:-1] for line in data[1:]]
-
-df = pd.DataFrame(data, columns=columns)
-
-
-date = np.asarray([d.split('-') for d in df['Date']])
-  
-df['Year'] = date[:,0]
-df['Month'] = date[:,1]
-days = np.arange(1,len(date)+1)
-df['Non-chronological day'] = days
+def ecobeeDataFrame(path):
+    '''
+    Description: It creates a data frame from a ecobee report csv table and includes 3 new columns with year, month and the non-chronological day.
+    
+        Input:
+            path: The path of the report file.
+        Output:
+            It returns a pandas data frame.
+    '''
+    
+    # just open file and read the data
+    file = open(path, 'r')
+    data = file.readlines()
+    file.close()
+    
+    # clean up the data removing empty rows and commentaries
+    i = 0;
+    while i < len(data):
+        if '#' in data[i] or '\n' == data[i] or '' == data[i]:
+            data.pop(i)
+        else:
+            i+=1
+    
+    # find the date of each row
+    columns = data[0].split(',')
+    data = [line.split(',')[:-1] for line in data[1:]]
+    df = pd.DataFrame(data, columns=columns)
+    date = np.asarray([d.split('-') for d in df['Date']])
+      
+    # create new coluns
+    df['Year'] = date[:,0]
+    df['Month'] = date[:,1]
+    days = np.arange(1,len(date)+1)
+    df['Non-chronological day'] = days
+    
+    return df
