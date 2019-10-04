e8f833be983691d05c5a5853a6f178b257f3adea
Guilherme Abreu <guiii88@outlook.com>
2019-10-04 18:07:00 -0300

    Now, It is necessary to get the max temperatures after preprocessing step

--- processing-tests.py
+++ processing-tests.py
@@ -12,7 +12,7 @@ import ecobee.preprocessing as pp
 flag     = '0 (not HW)/ 1 (HW)'
 hw_name  = 'HeatWaves'
 year_col = 'Year'
-day_col  = 'Julian day'
+day_col  = 'Days in Order'
 temp_col = 'Max Temperature'
 tt_temp  = 'Outdoor Temp (C)'
 
@@ -20,13 +20,15 @@ print('Generating Data Frame...')
 for i in range(1,7):
 
     try:
-        path = '../data_set/ecobee/House 1/Temperature/'+str(i)+'.csv'
+        path = '../data_set/ecobee/House1/'+str(i)+'.csv'
         if i == 1:
             df = pp.ecobeeDataFrame(path)
         else:
-            df = pp.appNewData(df,path)
+            df = pp.appendNewData(df,path)
     except:
         print('Something went wrong with this file: '+str(i)+'.csv')
+df[day_col] -= df[day_col].min() - 1
+df = pp.getmxtmp(df)
 print('Done!')
 
 print('Finding Heatwaves...')
@@ -35,9 +37,8 @@ fv = hwf.get_heatwave(data=df,
                       hw_name=hw_name, 
                       day_name = day_col, 
                       year_name = year_col,
-                      max_tmp_name = temp_col,
-                      min_tmp_name = temp_col)
+                      max_tmp_name = temp_col)
 print('Done!')
 
-res_df = fv[['Year','Julian day',tt_temp,temp_col,flag,hw_name, 'p90_max', 'p90_min']]
+res_df = fv[[day_col,temp_col,hw_name,'p90_max']]
 print('Heatwaves encountered: ', res_df[hw_name].max())
