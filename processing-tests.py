a3bcc4cb086c81cd2c7476ee6f933ba49eebb27a
Guilherme Abreu <guiii88@outlook.com>
2019-10-03 22:29:31 -0300

    Make new tests with the new features added to ecfobee library(appNewData, Julian day column

--- processing-tests.py
+++ processing-tests.py
@@ -5,42 +5,39 @@ Created on Wed Sep 18 14:57:37 2019
 
 @author: guilherme
 """
-
 #importing libraries
 import climate.heatwaveFinder as hwf
 import ecobee.preprocessing as pp
-    
-df_dict = dict()
-for year in range(1997, 2019):
-    
-    print('processing data: ' + str(year)+'.xlsx'+' ...')
-    
+
+flag     = '0 (not HW)/ 1 (HW)'
+hw_name  = 'HeatWaves'
+year_col = 'Year'
+day_col  = 'Julian day'
+temp_col = 'Max Temperature'
+tt_temp  = 'Outdoor Temp (C)'
+
+print('Generating Data Frame...')
+for i in range(1,7):
+
     try:
-        path = '../data_set/ecobee/House 1/Temperature/1.csv'
-        df = pp.ecobeeDataFrame(path)
-        flag = '0 (not HW)/ 1 (HW)'
-        hw_name='HeatWaves'
-        year_col = 'Year'
-        day_col = 'Non-chronological day'
-        temp_col = 'Max Temperature'
-        tt_temp = 'Outdoor Temp (C)'
-        
-        fv = hwf.get_heatwave(data=df, 
-                              flag=flag, 
-                              hw_name=hw_name, 
-                              day_name = day_col, 
-                              year_name = year_col,
-                              max_tmp_name = temp_col,
-                              min_tmp_name = temp_col)
-        
-        main_col = ['Year','Day',tt_temp,temp_col,flag,hw_name, 'p90_max', 'p90_min']
-        res_df = fv[main_col]
-        
-        df_dict[str(year)] = res_df
-        
-        print('data: ' + str(year)+'.xlsx'+' was processed.')
+        path = '../data_set/ecobee/House 1/Temperature/'+str(i)+'.csv'
+        if i == 1:
+            df = pp.ecobeeDataFrame(path)
+        else:
+            df = pp.appNewData(df,path)
     except:
-        print('Something went wrong with this file: '+str(year)+'.xlsx')
-        
-for year in df_dict:
-    df_dict[year].to_excel("hw_"+year+".xlsx")
+        print('Something went wrong with this file: '+str(i)+'.csv')
+print('Done!')
+
+print('Finding Heatwaves...')
+fv = hwf.get_heatwave(data=df, 
+                      flag=flag, 
+                      hw_name=hw_name, 
+                      day_name = day_col, 
+                      year_name = year_col,
+                      max_tmp_name = temp_col,
+                      min_tmp_name = temp_col)
+print('Done!')
+
+res_df = fv[['Year','Julian day',tt_temp,temp_col,flag,hw_name, 'p90_max', 'p90_min']]
+print('Heatwaves encountered: ', res_df[hw_name].max())
