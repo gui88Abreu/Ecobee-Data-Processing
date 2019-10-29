3693a2a538498f6e3190bf9cde4d06ce1937c065
Guilherme Abreu <guiii88@outlook.com>
2019-10-29 16:03:39 -0300

    It adjust tests to work with tha new macro names

--- processing-tests.py
+++ processing-tests.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Wed Sep 18 14:57:37 2019
+Created on Thu Sep 26 18:45:31 2019
 
-@author: guilherme
+@author: Guilherme de Brito Abreu, undergraduate student at @unicamp.
 """
 
 #importing libraries
@@ -14,9 +14,10 @@ path = '/home/guilherme/Documents/GIT REPOSITORIES/data_set/ecobee/003ad187b773a
 
 df = pp.ecobeeDataFrame(path)
 
-'''
+
 # generate a data frame from the ecobee csv file
 print('Generating Data Frame...')
+'''
 for i in range(1,11):
 
     try:
@@ -28,19 +29,18 @@ for i in range(1,11):
         quit('Something went wrong with this file! No such file or directory: '+path+str(i)+'.csv')
 '''
 # shift all days untill the first day be 1
-df[pp.nonCday] -= df[pp.nonCday].min() - 1
+df[pp.julianDayCol] -= df[pp.julianDayCol].min() - 1
 print('Done!')
 
 # select just the most important data e calculate important parameters like mean, max, and min temperatures
 print('Cleaning Up the Data...')
-cln_df = pp.cleanData(df)
+cln_df = pp.summarizeData(df)
 print('Done')
 
 print("Mean Temperature x Device On")
 pp.plot_TxD(cln_df)
 
-print("Day x Outside and Inside Temperature")
-pp.plot_DayxTcTo(cln_df)
+pp.plot_comparison(cln_df, ylabel = "Relative Humidity [%]", columns = [pp.meanOutHumCol, pp.meanInHumCol, pp.julianDayCol])
 
 '''
 It creates a copý of the original dataframe in order to have another 
@@ -48,11 +48,11 @@ dataframe that contains time colummn as numbers other than strings.
 It makes animated plot faster."
 '''
 new_df = df.copy()
-new_df[pp.timeName] = np.arange(5,new_df.shape[0]*5 + 1, 5)
+new_df[pp.timeCol] = np.arange(5,new_df.shape[0]*5 + 1, 5)
 
-pp.animated_plot(new_df[(new_df[pp.nonCday] >= 4) & (new_df[pp.nonCday] <= 8)],
+pp.animated_plot(new_df[(new_df[pp.julianDayCol] >= 4) & (new_df[pp.julianDayCol] <= 8)],
                         fileName   = 'tmp_animation.mp4',
-                         columns   = [pp.tName,pp.ctName, pp.timeName], 
+                         columns   = [pp.outTemCol,pp.inTemCol, pp.timeCol], 
                          nFrames   = 900, 
                          nInterval = 1000,
                          measlabel = "Temperature (C)",
@@ -61,9 +61,9 @@ pp.animated_plot(new_df[(new_df[pp.nonCday] >= 4) & (new_df[pp.nonCday] <= 8)],
                          ylabel    = "Indoor Temperature",
                          title     = "Comparison Between Outdoor and Indoor Temperature")
 
-pp.animated_plot_static(new_df[(new_df[pp.nonCday] >= 4) & (new_df[pp.nonCday] <= 8)],
+pp.animated_plot_static(new_df[(new_df[pp.julianDayCol] >= 4) & (new_df[pp.julianDayCol] <= 8)],
                         fileName   = 'tmp_animation2.mp4',
-                         columns   = [pp.tName,pp.ctName, pp.timeName], 
+                         columns   = [pp.outTemCol,pp.inTemCol, pp.timeCol], 
                          nFrames   = 900, 
                          nInterval = 500,
                          step      = 2,
