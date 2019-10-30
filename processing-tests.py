62c39fcba904b45ab3477673a1fe1e7dc5bd1b90
Guilherme Abreu <guiii88@outlook.com>
2019-10-30 17:45:52 -0300

    It Tests new class. Everithing is working fine. It is still missing tests with more than one file and animated plots

--- processing-tests.py
+++ processing-tests.py
@@ -8,67 +8,22 @@ Created on Thu Sep 26 18:45:31 2019
 
 #importing libraries
 import ecobee.preprocessing as pp
-import numpy as np
+import ecobee.metadata as md
 
-path = '/home/guilherme/Documents/GIT REPOSITORIES/data_set/ecobee/003ad187b773affeb756ebcdb56f14594b814509.csv'
+datasetPath = '../data_set/ecobee/'
 
-df = pp.ecobeeDataFrame(path)
-
+metadata = md.metaData(datasetPath+"meta_data.csv")
 
 # generate a data frame from the ecobee csv file
 print('Generating Data Frame...')
-'''
-for i in range(1,11):
-
-    try:
-        if i == 1:
-            df = pp.ecobeeDataFrame(path+str(i)+'.csv')
-        else:
-            df = pp.appendNewData(df,path+str(i)+'.csv')
-    except FileNotFoundError:
-        quit('Something went wrong with this file! No such file or directory: '+path+str(i)+'.csv')
-'''
-# shift all days untill the first day be 1
-df[pp.julianDayCol] -= df[pp.julianDayCol].min() - 1
+ecobee = pp.ecobeeData(datasetPath+"2018/00994395a299fdb2f2377879bec134d0047f6267.csv")
 print('Done!')
 
 # select just the most important data e calculate important parameters like mean, max, and min temperatures
 print('Cleaning Up the Data...')
-cln_df = pp.summarizeData(df)
+ecobee.summarizeData()
 print('Done')
 
-print("Mean Temperature x Device On")
-pp.plot_TxD(cln_df)
+ecobee.plot_TxD()
 
-pp.plot_comparison(cln_df, ylabel = "Relative Humidity [%]", columns = [pp.meanOutHumCol, pp.meanInHumCol, pp.julianDayCol])
-
-'''
-It creates a copý of the original dataframe in order to have another 
-dataframe that contains time colummn as numbers other than strings.
-It makes animated plot faster."
-'''
-new_df = df.copy()
-new_df[pp.timeCol] = np.arange(5,new_df.shape[0]*5 + 1, 5)
-
-pp.animated_plot(new_df[(new_df[pp.julianDayCol] >= 4) & (new_df[pp.julianDayCol] <= 8)],
-                        fileName   = 'tmp_animation.mp4',
-                         columns   = [pp.outTemCol,pp.inTemCol, pp.timeCol], 
-                         nFrames   = 900, 
-                         nInterval = 1000,
-                         measlabel = "Temperature (C)",
-                         tlabel    = "Time Diference (min)",
-                         xlabel    = "Outdoor Temperature",
-                         ylabel    = "Indoor Temperature",
-                         title     = "Comparison Between Outdoor and Indoor Temperature")
-
-pp.animated_plot_static(new_df[(new_df[pp.julianDayCol] >= 4) & (new_df[pp.julianDayCol] <= 8)],
-                        fileName   = 'tmp_animation2.mp4',
-                         columns   = [pp.outTemCol,pp.inTemCol, pp.timeCol], 
-                         nFrames   = 900, 
-                         nInterval = 500,
-                         step      = 2,
-                         measlabel = "Temperature (C)",
-                         tlabel    = "Time (min)",
-                         xlabel    = "Outdoor Temperature",
-                         ylabel    = "Indoor Temperature",
-                         title     = "Comparison Between Outdoor and Indoor Temperature")
+ecobee.plot_comparison(summ = True, ylabel = "Relative Humidity [%]", columns = [pp.meanOutHumCol, pp.meanInHumCol, pp.julianDayCol])
