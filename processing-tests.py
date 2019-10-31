46ba53f3a03339e99ba004b8cb579084d6409154
Guilherme Abreu <guiii88@outlook.com>
2019-10-31 18:55:17 -0300

    It tests multiple downloading files

--- processing-tests.py
+++ processing-tests.py
@@ -13,6 +13,7 @@ account     = 'g173691@dac.unicamp.br'
 #importing libraries
 import ecobee.preprocessing as pp
 import ecobee.metadata as md
+import numpy as np
 
 import os
 
@@ -25,19 +26,25 @@ x = metadata.data[metadata.data[md.frstCnctCol] == '00:00.0']
 file = (x[md.dataIdCol].values)[0] + '.csv'
 del x
 
-os.system('gsutil cp gs://donate_your_data_2019/files/2016/'+file+'.zip 2016')
-os.system('unzip 2016/'+file+'.zip -d 2016/')
+for year in np.arange(2015,2020,1).astype('str'):
+    os.system('gsutil cp gs://donate_your_data_2019/files/'+year+'/'+file+'.zip '+year)
+    os.system('unzip '+year+'/'+file+'.zip -d ' + year)
+
+ecobee = pp.ecobeeData()
 
 # generate a data frame from the ecobee csv file
 print('Generating Data Frame...')
-ecobee = pp.ecobeeData('2016/'+file)
+for year in np.arange(2015,2020,1).astype('str'):
+    ecobee.appendNewData(year+'/'+file)
 print('Done!')
 
 # select just the most important data e calculate important parameters like mean, max, and min temperatures
-print('Cleaning Up the Data...')
+print('Summerizing Data...')
 ecobee.summarizeData()
 print('Done')
 
 ecobee.plot_TxD()
 
-ecobee.plot_comparison(summ = True, ylabel = "Relative Humidity [%]", columns = [pp.meanOutHumCol, pp.meanInHumCol, pp.julianDayCol])
+ecobee.plot_comparison(summ = True, 
+                       ylabel = "Relative Humidity [%]", 
+                       columns = [pp.meanOutHumCol, pp.meanInHumCol, pp.julianDayCol])
