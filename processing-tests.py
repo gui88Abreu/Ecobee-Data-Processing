b60f42bbec61a559d5cd7a75f5ac49e0ea911b9f
Guilherme Abreu <guiii88@outlook.com>
2019-11-03 20:38:02 -0300

    It tests animated plot. It is still missing some tests

--- processing-tests.py
+++ processing-tests.py
@@ -9,7 +9,6 @@ Created on Thu Sep 26 18:45:31 2019
 datasetPath = '../data_set/ecobee/'
 account     = 'g173691@dac.unicamp.br'
 
-
 #importing libraries
 import ecobee.preprocessing as pp
 import ecobee.metadata as md
@@ -20,22 +19,23 @@ import os
 os.chdir(datasetPath)
 metadata = md.metaData("meta_data.csv")
 
-os.system('gcloud auth login '+ account)
-
 x = metadata.data[metadata.data[md.frstCnctCol] == '00:00.0']
-file = (x[md.dataIdCol].values)[0] + '.csv'
+device = (x[md.dataIdCol].values)[-1] 
+file =  device + '.csv'
 del x
 
+#download data from google cloud
+os.system('gcloud auth login '+ account)
 for year in np.arange(2015,2020,1).astype('str'):
     os.system('gsutil cp gs://donate_your_data_2019/files/'+year+'/'+file+'.zip '+year)
     os.system('unzip '+year+'/'+file+'.zip -d ' + year)
 
+# get the Data
 ecobee = pp.ecobeeData()
-
 # generate a data frame from the ecobee csv file
 print('Generating Data Frame...')
 for year in np.arange(2015,2020,1).astype('str'):
-    ecobee.appendNewData(year+'/'+file)
+    ecobee.append(year+'/'+file)
 print('Done!')
 
 # select just the most important data e calculate important parameters like mean, max, and min temperatures
@@ -43,8 +43,25 @@ print('Summerizing Data...')
 ecobee.summarizeData()
 print('Done')
 
-ecobee.plot_TxD()
-
-ecobee.plot_comparison(summ = True, 
-                       ylabel = "Relative Humidity [%]", 
+ecobee.plotTxD()
+ecobee.plotComparison(summ = True, 
+                       ylabel = "Air Mean Relative Humidity [%]", 
                        columns = [pp.meanOutHumCol, pp.meanInHumCol, pp.julianDayCol])
+
+new_df = ecobee.data.copy()
+new_df[pp.timeCol] = np.arange(5,new_df.shape[0]*5 + 1, 5)
+ecobee.animatedPlotStatic(dataframe = new_df[(new_df[pp.julianDayCol] >= 4) & (new_df[pp.julianDayCol] <= 8)],
+                fileName   = 'tmp_animation.mp4',
+                 columns   = [pp.outHumCol,pp.inHumCol, pp.timeCol], 
+                 nFrames   = 1000,
+                 nfps      = 60,
+                 step      = 2,
+                 nInterval = 500,
+                 measlabel = "Humidity (%)",
+                 tlabel    = "Time Diference (min)",
+                 xlabel    = "Outdoor Humidity",
+                 ylabel    = "Indoor Humidity",
+                 title     = "Comparison Between Outdoor and Indoor Humidity")
+
+deviceInfo = metadata.data.loc[metadata.data[md.dataIdCol] == device]
+print(deviceInfo.iloc[0])
